b989b2d4f4b9182b2e572743151a53538ffb6996
Gal Bitton <87198181+GalBitton@users.noreply.github.com>
2024-07-05 15:39:48 +0300

    Co-authored-by: Ely <EyalMK@users.noreply.github.com> Co-authored-by: Adir David <Adir111@users.noreply.github.com> Co-authored-by: nadavreu <nadavreu@users.noreply.github.com>

---
+++ setGit.bat
@@ -0,0 +1,7 @@
+git init
+echo "Web Project with Tailwind, VanillaJS - analytics of wearable health tracking devices" >> README.md
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/GalBitton/Web-Project.git
+git push -u origin main
